3ba3918d8135581a7db55ad406ae6003be7341a6
Mostafa Kamel <mkamel30@gmail.com>
2026-03-18 14:06:26 +0200

    payload_fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const io = new Server(server, {
 
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 
 // Routes
